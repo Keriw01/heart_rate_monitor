dcb977ad886a9ed5846526e0a87e926e493a408a
Keriw01 <sebas342@o2.pl>
2022-10-23 15:41:37 +0200

    Login Page View without auth

---
+++ lib/pages/sign_up.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class SignUp extends StatelessWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Sign UP');
+  }
+}
